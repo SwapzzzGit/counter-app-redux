05ba0f5439faae3f297475593fe76ac3516be66f
Swapnil Mundaware <93301234+SwapzzzGit@users.noreply.github.com>
2023-05-07 23:22:20 +0530

    2. Used Redux - Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h1>counter : 0</h1>
-      <button>Increment</button>
+      <h1>counter : {counter}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
     </div>
   );
 }
